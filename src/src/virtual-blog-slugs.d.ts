939fe805589f639a57c70629b119d68612932c9d
kryxen <kryxen.dev@gmail.com>
2026-04-09 22:53:56 +0530

    added Readme

--- src/src/virtual-blog-slugs.d.ts
+++
@@ -1,9 +0,0 @@
-type BlogSlugEntry = {
-  slug: string;
-  url: string;
-};
-
-declare module "virtual:blog-slugs" {
-  const blogSlugs: BlogSlugEntry[];
-  export default blogSlugs;
-}
---
+++ vshell/README.md
@@ -0,0 +1,2 @@
+# A shell emulator to run on my webiste
+this is not to solve any problem , i am just building this for fun 
